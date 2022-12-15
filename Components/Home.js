64e1ff19dd25bcd4796e0c4c1629ed6a9a216833
emonShimoul <emonshimoul@gmail.com>
2022-12-16 01:01:28 +0600

    added a button

--- Components/Home.js
+++ Components/Home.js
@@ -1,10 +1,17 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, Button } from 'react-native'
 import React from 'react'
 
 export default function Home() {
     return (
         <View>
             <Text>This is a home</Text>
+            <Button
+                onPress={() => {
+                    alert('You tapped the button!');
+                }}
+                title="Press Me"
+                color="#841584"
+            />
             <TextInput
                 style={{
                     height: 40,
