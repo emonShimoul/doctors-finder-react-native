0c017749f372e764575e7d8ce022f08098d0ff21
emonShimoul <emonshimoul@gmail.com>
2022-12-15 23:08:53 +0600

    add a component

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './Components/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello React Native!!</Text>
+      <Text>Hello!!</Text>
       <StatusBar style="auto" />
+      <Home></Home>
     </View>
   );
 }
