71f617f4299da55e5951e6b2596a317357be90f4
emonShimoul <emonshimoul@gmail.com>
2022-12-16 00:59:32 +0600

    use text input

--- Components/Home.js
+++ Components/Home.js
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
 export default function Home() {
     return (
         <View>
             <Text>This is a home</Text>
+            <TextInput
+                style={{
+                    height: 40,
+                    borderColor: 'gray',
+                    borderWidth: 1
+                }}
+                defaultValue="You can type in me"
+            />
         </View>
     )
 }
